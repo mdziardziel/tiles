285603e1055edb088c3848cd581518be901c201f
mdziardziel <mdziardziel@gmail.com>
2020-03-26 17:45:17 +0100

    remove image on hover

--- prop7.js
+++ prop7.js
@@ -1,8 +1,8 @@
 
 function swipeTilesRowsDown(){
-  let template = $("middle").css("grid-template-areas")
-  template = template.substring(1, template.length - 1)
-  let rows = template.split("\" \"")
+  // let template =
+  // template = template.substring(1, template.length - 1)
+  let rows =  getGridTemplateArea()
   let last_row = rows.pop()
   rows.unshift(last_row)
   let joined = rows.join("\" \"")
@@ -10,13 +10,18 @@ function swipeTilesRowsDown(){
 }
 
 function swipeTilesRowsUp(){
-  let template = $("middle").css("grid-template-areas")
-  template = template.substring(1, template.length - 1)
-  let rows = template.split("\" \"")
+  // let template = getGridTemplateArea()
+  // template = template.substring(1, template.length - 1)
+  let rows =  getGridTemplateArea()
   let first_row = rows.shift()
   rows.push(first_row)
   let joined = rows.join("\" \"")
   $("middle").css("grid-template-areas", "\"" + joined + "\"")
+}
+
+function getGridTemplateArea(){
+  let template = $("middle").css("grid-template-areas")
+  return template.substring(1, template.length - 1).split("\" \"")
 }
 
 $(window).on('load', function() {
@@ -32,4 +37,44 @@ $(window).on('load', function() {
   $(window).resize(function() {
     $("middle").removeAttr("style")
     });
+  
+  $('.rowclick').hover( function() {
+    let id = $(this).attr('id')
+    // let template = getGridTemplateArea()
+    let templateRows =  getGridTemplateArea()
+    let rowId = templateRows.findIndex(element => element.includes(id))
+    let divIds = templateRows[rowId].split(" ")
+    // let selector = '#' + divIds.join(', #') // for all row
+    let selector = '#' + id // for selected 
+
+    let size = $(selector).css('background-size') 
+
+    let new_size = null;
+    if(size == 'cover') {
+      new_size = '0px 0px'
+    } else {
+      new_size = 'cover'
+    }
+    $(selector).css('background-size', new_size)
+
+    // $('#' + id).css('min-width', '500px')
+  });
+
+  $('.rowclick').click( function() {
+    let id = $(this).attr('id')
+    // let template = getGridTemplateArea()
+    let templateRows =  getGridTemplateArea()
+    let rowId = templateRows.findIndex(element => element.includes(id))
+    let divIds = templateRows[rowId].split(" ")
+    // let selector = '#' + divIds.join(', #') // for all row
+    let selector = '#' + id // for selected 
+
+
+    let available_colors = ['yellow', 'blue', 'red', 'green', 'orange']
+    let color = available_colors[Math.floor(Math.random()*available_colors.length)]
+    console.log(color)
+    $(selector).css('background-color', color)
+
+    // $('#' + id).css('min-width', '500px')
+  });
 });
