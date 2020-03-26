700a9e4607244dd4657c3172ac7d16612dec1887
mdziardziel <mdziardziel@gmail.com>
2020-03-26 23:12:31 +0100

    clean code

--- prop7.js
+++ prop7.js
@@ -1,22 +1,16 @@
 
 function swipeTilesRowsDown(){
-  // let template =
-  // template = template.substring(1, template.length - 1)
   let rows =  getGridTemplateArea()
   let last_row = rows.pop()
   rows.unshift(last_row)
-  let joined = rows.join("\" \"")
-  $("middle").css("grid-template-areas", "\"" + joined + "\"")
+  setGridTemplate(rows)
 }
 
 function swipeTilesRowsUp(){
-  // let template = getGridTemplateArea()
-  // template = template.substring(1, template.length - 1)
   let rows =  getGridTemplateArea()
   let first_row = rows.shift()
   rows.push(first_row)
-  let joined = rows.join("\" \"")
-  $("middle").css("grid-template-areas", "\"" + joined + "\"")
+  setGridTemplate(rows)
 }
 
 function getGridTemplateArea(){
@@ -24,7 +18,61 @@ function getGridTemplateArea(){
   return template.substring(1, template.length - 1).split("\" \"")
 }
 
-$(window).on('load', function() {
+function setGridTemplate(rows) {
+  let joined = rows.join("\" \"")
+  $("middle").css("grid-template-areas", "\"" + joined + "\"")
+}
+
+function colorizeTilesAtStrat() {
+  $('.rowclick').each( function() { setRandomColorsFor(this) })
+}
+
+function setRandomColorsFor(selector) {
+  let available_colors = ['rgba(183,28,28', 'rgba(136,14,79', 'rgba(74,20,140', 'rgba(49,27,146', 'rgba(26,35,126', 'rgba(13,71,161', 'rgba(1,87,155', 'rgba(0,96,100', 'rgba(0,77,64', 'rgba(27,94,32', 'rgba(51,105,30', 'rgba(130,119,23']
+  let color = available_colors[Math.floor(Math.random()*available_colors.length)]
+  let opacity = ((Math.floor(Math.random() * 5)/10) + 0.5 ).toString();
+  $(selector).css('box-shadow', 'inset 0 0 0 1000px ' + color +', ' + opacity + ')' )
+}
+
+function newTemplateFilledWithOneId(id, rowsNumber, columnsNumber) {
+  let row = "\"" + id + (" " + id).repeat(columnsNumber - 1) + "\""
+  let template = row + (" " + row).repeat(rowsNumber - 1)
+
+  return template;
+}
+
+function selectorWithAllIdsExceptClickedId(id, templateRows) {
+  let rowsNumber = templateRows.length
+
+  let selector = ''
+  var i;
+  for (i = 0; i < rowsNumber; i++) {
+    selector += "#" + templateRows[i].split(" ").join(', #')
+    if(i != rowsNumber - 1) { selector += ', ' }
+  }
+
+  selector = selector.split("#" + id + ',').join('').split("#" + id).join('')
+
+  return selector;
+}
+
+function changeOpacity(opacity, opacityStep, template, selector, id) {
+  if(opacity >= opacityStep){
+    $(selector).css('opacity', opacity)
+    setTimeout(function() { changeOpacity(opacity - opacityStep, opacityStep, template, selector, id) }, 20);
+  } else {
+    // resize tile
+    $(selector).css('display', 'none')
+    $("middle").css("grid-template-areas", template)
+    $('#' + id).css("width", '100%')
+    $('#' + id).css("height", '100%')
+    $('#' + id).css("padding", '0px')
+  }
+}
+
+
+$(window).on('DOMContentLoaded', function() {
+  colorizeTilesAtStrat()
 
   // --------- FEATURE 1 TILES SLIDER ---------------
   $(document).keypress(function(event) {
@@ -36,112 +84,36 @@ $(window).on('load', function() {
     }
   });
 
-    // clean tiles order after resize 
-  $(window).resize(function() {
-    $("middle").removeAttr("style")
-    });
+  $(window).resize(function() { $("middle").removeAttr("style") });   // clean grid template after resize 
   // --------- FEATURE 1 -----------------
-
-  
 
   // ---------- FEATURE 2 CHANGE TILES COLOR ON ------------------
   $('.rowclick').mouseover( function() {
     let id = $(this).attr('id')
-    // let template = getGridTemplateArea()
-    let templateRows =  getGridTemplateArea()
-    let rowId = templateRows.findIndex(element => element.includes(id))
-    let divIds = templateRows[rowId].split(" ")
-    // let selector = '#' + divIds.join(', #') // for all row
-    let selector = '#' + id // for selected 
-
-
-    let available_colors = ['rgba(183,28,28', 'rgba(136,14,79', 'rgba(74,20,140', 'rgba(49,27,146', 'rgba(26,35,126', 'rgba(13,71,161', 'rgba(1,87,155', 'rgba(0,96,100', 'rgba(0,77,64', 'rgba(27,94,32', 'rgba(51,105,30', 'rgba(130,119,23']
-    let color = available_colors[Math.floor(Math.random()*available_colors.length)]
-    let opacity = ((Math.floor(Math.random() * 5)/10) + 0.5 ).toString();
-    $(selector).css('box-shadow', 'inset 0 0 0 1000px ' + color +', ' + opacity + ')' )
-
-    // $('#' + id).css('min-width', '500px')
+    let selector = '#' + id
+    setRandomColorsFor(selector)
   });
   // ----------- FEATURE 2 -----------------------------------
-
-
-  // COLORIZE TILES AT START
-  function colorizeTilesAtStrat() {
-    $('.rowclick').each( function() {
-      let available_colors = ['rgba(183,28,28', 'rgba(136,14,79', 'rgba(74,20,140', 'rgba(49,27,146', 'rgba(26,35,126', 'rgba(13,71,161', 'rgba(1,87,155', 'rgba(0,96,100', 'rgba(0,77,64', 'rgba(27,94,32', 'rgba(51,105,30', 'rgba(130,119,23']
-      let color = available_colors[Math.floor(Math.random()*available_colors.length)]
-      let opacity = ((Math.floor(Math.random() * 5)/10) + 0.5 ).toString();
-      // console.log(color)
-      $(this).css('box-shadow', 'inset 0 0 0 1000px ' + color +', ' + opacity + ')' )
-    })
-  }
-  colorizeTilesAtStrat()
-  // COLORIZE TILES
 
 
   // --------------- FEATURE 3 ENLARGE TILE ------------------------------
   let resetCss = false
   $('.rowclick').click( function() {
     let id = $(this).attr('id')
-    // let template = getGridTemplateArea()
     let templateRows =  getGridTemplateArea()
     let rowsNumber = templateRows.length
-    let rowId = templateRows.findIndex(element => element.includes(id))
-    let divIds = templateRows[rowId].split(" ")
-    let columnsNumber = divIds.length
+    let columnsNumber = templateRows[0].split(" ").length
 
-    // new template
-    let row = "\"" + id + (" " + id).repeat(columnsNumber - 1) + "\""
-    let template = row + (" " + row).repeat(rowsNumber - 1)
-
-    // console.log(template)
-    // $("middle").css("grid-template-areas", "\"" + id + "\"")
-
-    let selector = ''
-    var i;
-    for (i = 0; i < rowsNumber; i++) {
-      selector += "#" + templateRows[i].split(" ").join(', #')
-      if(i != rowsNumber - 1) { selector += ', ' }
-    }
-
-    selector = selector.split("#" + id + ',').join('').split("#" + id).join('')
-
-    // change opacity step by step
     let opacity = 1;
     let opacityStep = 0.01
+    let template = newTemplateFilledWithOneId(id, rowsNumber, columnsNumber)
+    let selector = selectorWithAllIdsExceptClickedId(id, templateRows)
 
-    function changeOpacity() {
-      if(opacity >= opacityStep){
-        opacity -= opacityStep
-        $(selector).css('opacity', opacity)
-        setTimeout(changeOpacity, 20);
-      } else {
-        // resize tile
-        $(selector).css('display', 'none')
-        $("middle").css("grid-template-areas", template)
-        $('#' + id).css("width", '100%')
-        $('#' + id).css("height", '100%')
-        $('#' + id).css("padding", '0px')
-        resetCss = true
-      }
-    }
-
-    changeOpacity()
-
-    // let selector = '#' + divIds.join(', #') // for all row
-    // let selector = '#' + id // for selected 
-
-
-    // let available_colors = ['yellow', 'blue', 'red', 'green', 'orange']
-    // let color = available_colors[Math.floor(Math.random()*available_colors.length)]
-    // console.log(color)
-    // $(selector).css('background-color', color)
-
-    // $('#' + id).css('min-width', '500px')
+    changeOpacity(opacity, opacityStep, template, selector, id)
+    resetCss = true
   });
 
-      // clean tiles order after resize 
-  $(window).resize(function() {
+  $(window).resize(function() { // clean tiles style after resize 
     if(resetCss) {
       $(".rowclick").removeAttr("style")
       colorizeTilesAtStrat()
